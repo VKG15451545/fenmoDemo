5147083e16ad3834d7a9361db985004b741b39e6
Vikas Kumar Gupta <vkg15451545@gmail.com>
2026-04-20 16:01:06 +0530

    Initial commit
    # Conflicts:
    #	.gitignore

---
+++ src/main/java/com/example/fenmoDemo/FenmoDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.fenmoDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FenmoDemoApplication {
+
+	public static void main(String[] args) {
+        SpringApplication.run(FenmoDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/fenmoDemo/FenmoDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fenmoDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FenmoDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
